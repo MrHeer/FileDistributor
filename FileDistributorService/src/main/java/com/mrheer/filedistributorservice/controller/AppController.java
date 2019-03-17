08b6c543c40a586a90d19257886cfb67f8be527d
MrHeer <hlm52pk@163.com>
2019-03-17 16:04:58 +0800

    add error page

--- FileDistributorService/src/main/java/com/mrheer/filedistributorservice/controller/AppController.java
+++ FileDistributorService/src/main/java/com/mrheer/filedistributorservice/controller/AppController.java
@@ -1,11 +1,12 @@
 package com.mrheer.filedistributorservice.controller;
 
+import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-public class AppController {
+public class AppController implements ErrorController {
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String index() {
         return "index.html";
@@ -18,6 +19,16 @@ public class AppController {
 
     @RequestMapping(value = "/HostManage", method = RequestMethod.GET)
     public String hostManage() {
+        return "index.html";
+    }
+
+    @RequestMapping(value = "/error", method = RequestMethod.GET)
+    public String error() {
+        return "index.html";
+    }
+
+    @Override
+    public String getErrorPath() {
         return "index.html";
     }
 }
