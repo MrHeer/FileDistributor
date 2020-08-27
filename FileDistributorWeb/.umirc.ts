317bca498423782285abd873ecfbc35b6ef411b3
MrHeer <hlm52pk@163.com>
2020-08-27 17:39:58 +0800

    upgrade: update umi

---
+++ FileDistributorWeb/.umirc.ts
@@ -0,0 +1,56 @@
+import { defineConfig } from "umi";
+
+export default defineConfig({
+  routes: [
+    {
+      path: "/",
+      component: "../layouts/App",
+      routes: [
+        {
+          path: "/",
+          redirect: "/FileDistribute",
+        },
+        {
+          path: "/FileDistribute",
+          component: "FileDistribute",
+        },
+        {
+          path: "/FileManage",
+          component: "FileManage",
+        },
+        {
+          path: "/HostManage",
+          component: "HostManage",
+        },
+        {
+          path: "/exception/403",
+          component: "403",
+        },
+        {
+          path: "/exception/404",
+          component: "404",
+        },
+        {
+          path: "/exception/500",
+          component: "500",
+        },
+        {
+          component: "404",
+        },
+      ],
+    },
+  ],
+  antd: true,
+  dva: true,
+  dynamicImport: true,
+  locale: {
+    baseNavigator: true, // 为true时，用navigator.language的值作为默认语言
+    antd: true, // 是否启用antd的<LocaleProvider />
+  },
+  proxy: {
+    "/api": {
+      target: "http://localhost:8888/",
+      changeOrigin: true,
+    },
+  },
+});
